6d0a04e6f647ddd3272b8e2ebbfdf270bad8034c
ddtddt <ddtddt@piwigo.org>
2011-07-13 18:44:21 +0000

    [extensions] - showcase_subscribe - add cs_CZ - Česky - Czech thanks to webprostor.eu
    git-svn-id: http://piwigo.org/svn/extensions/showcase_subscribe@11733 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/cs_CZ/plugin.lang.php
@@ -0,0 +1,11 @@
+<?php
+$lang['Subscribe my gallery to Piwigo Showcase'] = 'Přihlaš mojí galerii do Piwigo Showcase';
+$lang['Gallery Address (URL)'] = 'URL odkaz Galerie';
+$lang['An error has occured, no connection to %s'] = 'Nastala chyba, není spojení s %s';
+$lang['Your subscription is currently pending, if you have provided an email, you will be notified as soon as your gallery is registered'] = 'Vaše přihláška je zpracovávána, pokud máte email, budete upozorněn jakmile bude vaše galerie zaregistrována';
+$lang['Your gallery is already registered in Piwigo Showcase, <a href="%s">see it →</a>'] = 'Vaše galerie je již zaregistrována v Piwigo Showcase, <a href="%s">podívejte se →</a>';
+$lang['A good description will improve your visibility in search engines'] = 'Dobrý popis zlepší vidtelnost vaší galerie pro vyhledávače';
+$lang['provide it if you want to be notified when your gallery is registered'] = 'poskytněte ho pokud chcete být informováni až bude galerie zaregistrována';
+$lang['(optional)'] = '(volitelné)';
+$lang['(example: nature, landscape, portrait)'] = '(např.: příroda, krajina, portrét)';
+?>
