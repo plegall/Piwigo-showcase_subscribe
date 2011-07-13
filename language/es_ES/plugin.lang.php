2e9a3b62bca4ae190d88d9baa025f779840985ae
ddtddt <ddtddt@piwigo.org>
2011-07-13 18:09:54 +0000

    [extensions] - showcase_subscribe - add  es_ES - Español - Spanish thanks to jpr928
    git-svn-id: http://piwigo.org/svn/extensions/showcase_subscribe@11731 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/es_ES/plugin.lang.php
@@ -0,0 +1,11 @@
+<?php
+$lang['Subscribe my gallery to Piwigo Showcase'] = 'Registrar mi galería en el directorio de listado de Piwigo';
+$lang['Gallery Address (URL)'] = 'Dirección de la galería (URL)';
+$lang['An error has occured, no connection to %s'] = 'Se produjo un error, no hay conexión en %s';
+$lang['Your subscription is currently pending, if you have provided an email, you will be notified as soon as your gallery is registered'] = 'Su inscripción está actualmente en espera, si a proporcionado una dirección de correo electrónico, se le notificará cuando su galería estara registrada';
+$lang['Your gallery is already registered in Piwigo Showcase, <a href="%s">see it →</a>'] = 'Su galería ya está registrado en el directorio de listado de Piwigo, <a href="%s">verla →</a>';
+$lang['A good description will improve your visibility in search engines'] = 'Una buena descripción mejorará su visibilidad en los motores de búsqueda';
+$lang['provide it if you want to be notified when your gallery is registered'] = 'dar uno si desea ser notificado cuando la galería se guardará';
+$lang['(optional)'] = '(opcional)';
+$lang['(example: nature, landscape, portrait)'] = '(ejemplo: naturaleza paisaje, retrato)';
+?>
