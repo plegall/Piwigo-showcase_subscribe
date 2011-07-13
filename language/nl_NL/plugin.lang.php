ce7243bdec8de940d1830b807ce2a5e90503a7d7
ddtddt <ddtddt@piwigo.org>
2011-07-13 18:14:39 +0000

    [extensions] - showcase_subscribe - add NL (Dutch - Nederlands) thanks to kees
    git-svn-id: http://piwigo.org/svn/extensions/showcase_subscribe@11732 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,11 @@
+<?php
+$lang['Subscribe my gallery to Piwigo Showcase'] = 'Meldt mijn galerie aan bij Piwigo Showcase'; 
+$lang['Gallery Address (URL)'] = 'Galerie-adres (URL)'; 
+$lang['An error has occured, no connection to %s'] = 'Er is een fout opgetreden, geen verbinding met %s'; 
+$lang['Your subscription is currently pending, if you have provided an email, you will be notified as soon as your gallery is registered'] = 'Uw aanmelding is momenteel in behandeling. Indien u een email-adres heeft opgegeven krijgt u bericht zodra uw galerie is geregistreerd'; 
+$lang['Your gallery is already registered in Piwigo Showcase, <a href="%s">see it →</a>'] = 'Uw galerie is reeds aangemeld bij Piwigo Showcase, <a href="%s">bekijk de galerie →</a>'; 
+$lang['A good description will improve your visibility in search engines'] = 'Een goede beschrijving van uw galerie zal de zichtbaarheid bij zoekmachines vergroten'; 
+$lang['provide it if you want to be notified when your gallery is registered'] = 'Geef uw email-adres indien u gewaarschuwd wenst te worden wanneer uw galerie is ingeschreven'; 
+$lang['(optional)'] = '(optioneel)';
+$lang['(example: nature, landscape, portrait)'] = '(bijvoorbeeld: natuur, landschap, portret)';
+?>
