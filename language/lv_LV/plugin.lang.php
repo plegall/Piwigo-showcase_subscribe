ff4e4fd5899153c13164ac9dff1aeed043c2d652
ddtddt <ddtddt@piwigo.org>
2011-07-13 05:56:33 +0000

    [extensions] - showcase_subscribe - add LV (Latvian - Latviešu) thanks to Aivars Baldone
    git-svn-id: http://piwigo.org/svn/extensions/showcase_subscribe@11721 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/lv_LV/plugin.lang.php
@@ -0,0 +1,11 @@
+<?php
+$lang['Subscribe my gallery to Piwigo Showcase'] = 'Pieteikt savu galeriju uz Piwigo Showcase';
+$lang['Gallery Address (URL)'] = 'Galerijas Adrese (URL)';
+$lang['An error has occured, no connection to %s'] = 'Ir notikusi klūme, nav savienojuma ar %s';
+$lang['Your subscription is currently pending, if you have provided an email, you will be notified as soon as your gallery is registered'] = 'Jūsu pieteikums aizkavējas, ja esat uzrādījis e-pastu, jums tiks paziņots tiklīdz jūsu galerija būs piereģistrēta';
+$lang['Your gallery is already registered in Piwigo Showcase, <a href="%s">see it →</a>'] = 'Jūsu galerija jau ir piereģistrēta  Piwigo Showcase, <a href="%s">skati to →</a>';
+$lang['A good description will improve your visibility in search engines'] = 'Labs apraksts uzlabos jūsu atpazīstamību meklēšanas dziņos';
+$lang['provide it if you want to be notified when your gallery is registered'] = 'uzrādiet to, ja vēlaties, lai jums paziņo par jūsu galerijas reģistrāciju';
+$lang['(optional)'] = '(izvēles)';
+$lang['(example: nature, landscape, portrait)'] = '(piemērs: daba, ainava, portrets)';
+?>
