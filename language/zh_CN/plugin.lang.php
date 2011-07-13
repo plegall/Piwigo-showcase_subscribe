c9d07b02f4bddfba59fc913980e92dd8477b4646
ddtddt <ddtddt@piwigo.org>
2011-07-13 11:14:44 +0000

    [extensions] - showcase_subscribe - add zh_CN - 简体中文 - Chinese thanks to winson
    git-svn-id: http://piwigo.org/svn/extensions/showcase_subscribe@11723 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,11 @@
+<?php
+$lang['Subscribe my gallery to Piwigo Showcase'] = '将我的我的画廊订阅到Piwigo的展示橱窗';
+$lang['Gallery Address (URL)'] = '画廊地址(URL)';
+$lang['An error has occured, no connection to %s'] = '发生了一个错误，没有 %s 的连接';
+$lang['Your subscription is currently pending, if you have provided an email, you will be notified as soon as your gallery is registered'] = '您的订阅申请正在审批中，如果您提供了email，则您的申请当通过后会马上收到我们的通知。';
+$lang['Your gallery is already registered in Piwigo Showcase, <a href="%s">see it →</a>'] = '您的画廊已经被注册到了 Piwigo 的展示橱窗, <a href="%s">查看此处 →</a>';
+$lang['A good description will improve your visibility in search engines'] = '一个好的描述将会有利于您网站在搜索引擎里展现。';
+$lang['provide it if you want to be notified when your gallery is registered'] = '如果您想收到注册成功的相关通知，请提供它';
+$lang['(optional)'] = '(可选)';
+$lang['(example: nature, landscape, portrait)'] = '(例如：自然，风景，肖像)';
+?>
