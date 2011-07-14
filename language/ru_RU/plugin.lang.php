747f99acbffb6157ca4e77435e0cf845f2e16461
ddtddt <ddtddt@piwigo.org>
2011-07-14 23:03:38 +0000

    [extensions] - showcase_subscribe - add Ru (Русский - Russian) thanks to yuragal
    git-svn-id: http://piwigo.org/svn/extensions/showcase_subscribe@11749 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ru_RU/plugin.lang.php
@@ -0,0 +1,11 @@
+<?php
+$lang['Subscribe my gallery to Piwigo Showcase'] = 'Внести мою галерею в Piwigo Showcase';
+$lang['Gallery Address (URL)'] = 'Адрес галереи (URL)';
+$lang['An error has occured, no connection to %s'] = 'Ошибка: нет соединения с %s';
+$lang['Your subscription is currently pending, if you have provided an email, you will be notified as soon as your gallery is registered'] = 'Ваша регистрация не завершена. Если вы предоставили адрес электронной почты, то получите письмо после регистрации гелереи';
+$lang['Your gallery is already registered in Piwigo Showcase, <a href="%s">see it →</a>'] = 'Ваша галерея уже зарегистрирована в Piwigo Showcase, <a href="%s">посмотреть →</a>';
+$lang['A good description will improve your visibility in search engines'] = 'Хорошее описание увеличит индексируемость галереи поисковыми машинами';
+$lang['provide it if you want to be notified when your gallery is registered'] = 'Укажите адрес, если хотите получить уведомление о регистрации галереи';
+$lang['(optional)'] = '(не обязательно)';
+$lang['(example: nature, landscape, portrait)'] = '(например: природа, ландшафт, портрет)';
+?>
