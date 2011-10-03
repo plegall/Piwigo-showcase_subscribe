6b0e0bd144a703ca823c5f7241108c58dd76ac50
ddtddt <ddtddt@piwigo.org>
2011-10-03 21:24:45 +0000

    [extensions] - showcase_subscribe - add sk_SK – Slovensky – Slovak thanks to dodo
    git-svn-id: http://piwigo.org/svn/extensions/showcase_subscribe@12338 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sk_SK/plugin.lang.php
@@ -0,0 +1,11 @@
+<?php
+$lang['Subscribe my gallery to Piwigo Showcase'] = 'Registrácia mojej galérie na Piwigo Showcase'; 
+$lang['Gallery Address (URL)'] = 'Adresa galérie (URL)'; 
+$lang['An error has occured, no connection to %s'] = 'Nastala chyba, žiadna konektivita na %s'; 
+$lang['Your subscription is currently pending, if you have provided an email, you will be notified as soon as your gallery is registered'] = 'Vaša registrácia čaká, ak ste poskytli email, dostanete čoskoro oznam o registrácii Vašej galérie'; 
+$lang['Your gallery is already registered in Piwigo Showcase, <a href="%s">see it →</a>'] = 'Vaša galéria je registrovaná na Piwigo Showcase, <a href="%s">pozrite sa sem →</a>'; 
+$lang['A good description will improve your visibility in search engines'] = 'Dobrý popis vylepší viditeľnosť vo vyhľadávačoch'; 
+$lang['provide it if you want to be notified when your gallery is registered'] = 'poskytnite ho, ak chcete dostať oznam o registrácii Vašej galérie'; 
+$lang['(optional)'] = '(voliteľné)';
+$lang['(example: nature, landscape, portrait)'] = '(príklad: príroda, krajinka, portrét)';
+?>
