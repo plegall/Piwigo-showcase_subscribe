db241bbc0503b572756c0454f5d415d9a076fee5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2011-10-05 19:59:04 +0000

    [Showcase Subscribe] Update tr_TR, thanks to : Nakre
    git-svn-id: http://piwigo.org/svn/extensions/showcase_subscribe@12353 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/tr_TR/plugin.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2009 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['(example: nature, landscape, portrait)'] = '(örnek: doğa, manzara, portre)';
+$lang['(optional)'] = '(isteğe bağlı)';
+$lang['A good description will improve your visibility in search engines'] = 'İyi bir açıklama, arama motorlarında görünürlüğünüzü artıracaktır.';
+$lang['An error has occured, no connection to %s'] = 'Bir hata oluştu, %s ile bağlantı kurulamadı';
+$lang['Gallery Address (URL)'] = 'Galeri Bağlantısı(URL)';
+$lang['provide it if you want to be notified when your gallery is registered'] = 'Galeriniz, Piwigo Sergisina kayıt edildiğinde haberdar edilmek istiyor musunuz ?';
+$lang['Subscribe my gallery to Piwigo Showcase'] = 'Galerimi Piwigo Sergisine kaydet';
+$lang['Your gallery is already registered in Piwigo Showcase, <a href="%s">see it →</a>'] = 'Galeriniz zaten Piwigo Sergisine ekli <a href="%s">görmek için tıklayınız →</a>';
+$lang['Your subscription is currently pending, if you have provided an email, you will be notified as soon as your gallery is registered'] = 'Kaydınız henüz bekleme aşamasında, eğer bir e-posta bildirdiysenizz, sergiye kayıt olduğunuzda size bir posta ile bildirilecektir.';
+?>
