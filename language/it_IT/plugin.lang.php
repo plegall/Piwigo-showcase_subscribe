e8095eb8d1c7d835561932f57a1fb424b2f9cba5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-02-25 12:13:32 +0000

    [Showcase Subscribe] Update it_IT, thanks to : Gdvpixel
    git-svn-id: http://piwigo.org/svn/extensions/showcase_subscribe@13338 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/it_IT/plugin.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['(example: nature, landscape, portrait)'] = '(esempio: natura, panorami, ritratti)';
+$lang['(optional)'] = '(opzionale)';
+$lang['A good description will improve your visibility in search engines'] = 'Una buona descrizione migliora la tua visibilità nei motori di ricerca';
+$lang['An error has occured, no connection to %s'] = 'Errore, impossibile connettersi per %s';
+$lang['Gallery Address (URL)'] = 'Indirizzo (URL) galleria';
+$lang['provide it if you want to be notified when your gallery is registered'] = 'Fornisci il tuo indirizzo se vuoi ricevere una notifica quando la tua galleria è registrata';
+$lang['Subscribe my gallery to Piwigo Showcase'] = 'Registra la mia galleria su Piwigo Showcase';
+$lang['Your gallery is already registered in Piwigo Showcase, <a href="%s">see it →</a>'] = 'La tua galleria è già registrata in Piwigo Showcase, <a href="%s">Guarda →</a>';
+$lang['Your subscription is currently pending, if you have provided an email, you will be notified as soon as your gallery is registered'] = 'La tua registrazione è in corso di approvazione, se hai inserito un indirizzo email, ti verrà notificato non appena la tua galleria è stata registrata';
+?>
