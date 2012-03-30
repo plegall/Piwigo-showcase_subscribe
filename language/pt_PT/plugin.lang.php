f2f0a23b3d713872ecb51421fe558dac240ae2dc
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-03-30 18:30:45 +0000

    [Showcase Subscribe] Add pt_PT, thanks to : klito
    git-svn-id: http://piwigo.org/svn/extensions/showcase_subscribe@13811 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_PT/plugin.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['(example: nature, landscape, portrait)'] = '(exemplo: Natureza, Paisagens,retrato, etc)';
+$lang['(optional)'] = '(opcional)';
+$lang['A good description will improve your visibility in search engines'] = 'Uma boa descrição aumentará a sua visibilidade nas máquinas de busca';
+$lang['An error has occured, no connection to %s'] = 'Ocorreu um erro na ligação com %s';
+$lang['Gallery Address (URL)'] = 'Direção da Galeria (URL)';
+$lang['provide it if you want to be notified when your gallery is registered'] = 'Faculte isto se deseja ser notificado(a)quando a sua galeria estiver registada';
+$lang['Subscribe my gallery to Piwigo Showcase'] = 'Registar a minha galeria no conteúdo mostruário do Piwigo';
+$lang['Your gallery is already registered in Piwigo Showcase, <a href="%s">see it →</a>'] = 'A sua Galeria está registada no conteúdo mostruário do Piwigo <a href="%s">Veja isto→</a>';
+$lang['Your subscription is currently pending, if you have provided an email, you will be notified as soon as your gallery is registered'] = 'O seu registo está actualmente pendente; se forneceu um email,será notificado(a)logo que a sua galeria seja registada.';
+?>
