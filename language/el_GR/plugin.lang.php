8d1f35ce36c8d81b3dca32c97a8a8678821c5fe1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-05-06 15:07:48 +0000

    [showcase_subscribe] Update el_GR, thanks to : bas_alba
    git-svn-id: http://piwigo.org/svn/extensions/showcase_subscribe@14804 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/el_GR/plugin.lang.php
+++ language/el_GR/plugin.lang.php
@@ -4,7 +4,7 @@ $lang['Gallery Address (URL)'] = 'Διεύθυνση γκαλλερί (URL)';
 $lang['An error has occured, no connection to %s'] = 'Παρουσιάστηκε σφάλμα σύνδεσης με το %s';
 $lang['Your subscription is currently pending, if you have provided an email, you will be notified as soon as your gallery is registered'] = 'Η εγγραφή σας εκκρεμεί προς το παρών, αν έχετε δώσει email, θα ειδοποιηθείτε αμέσως μόλις γκαλλερί σας καταχωρηθεί';
 $lang['Your gallery is already registered in Piwigo Showcase, <a href="%s">see it →</a>'] = 'Η γαλλερί έχει ήδη εγγραφεί στο Piwigo Showcase, <a href="%s">see it →</a>';
-$lang['A good description will improve your visibility in search engines'] = 'Μια καλή περιγραφή θα βελτιώσει την ορατότητα από τις μηχανές αναζήτησης';
+$lang['A good description will improve your visibility in search engines'] = 'Μια καλή περιγραφή θα βελτιώσει την ορατότητα στις μηχανές αναζήτησης';
 $lang['provide it if you want to be notified when your gallery is registered'] = 'Παρέχετε το εάν θέλετε να ενημερωθείτε για την καταχώρηση της γκαλλερί σας';
 $lang['(optional)'] = '(προαιρετικό)';
 $lang['(example: nature, landscape, portrait)'] = '(παράδειγμα: φύση, τοπίο, πορτραίτο)'; 
