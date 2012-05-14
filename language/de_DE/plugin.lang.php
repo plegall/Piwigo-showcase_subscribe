294f3ec3d09e0ad5738431237e3e430aa45f966d
duke <duke@piwigo.org>
2012-05-14 18:16:26 +0000

    fixed some spelling errors in german translation
    git-svn-id: http://piwigo.org/svn/extensions/showcase_subscribe@14973 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/de_DE/plugin.lang.php
+++ language/de_DE/plugin.lang.php
@@ -1,6 +1,6 @@
 <?php
 $lang['Subscribe my gallery to Piwigo Showcase'] = 'Registriere meine Galerie bei Piwigo Showcase'; 
-$lang['Gallery Address (URL)'] = 'Gallerieadresse (URL)'; 
+$lang['Gallery Address (URL)'] = 'Galerieadresse (URL)'; 
 $lang['An error has occured, no connection to %s'] = 'Ein Fehler ist aufgetreten, %s ist nicht erreichbar'; 
 $lang['Your subscription is currently pending, if you have provided an email, you will be notified as soon as your gallery is registered'] = 'Ihre Registrierung ist momentan im Wartestatus, wenn Sie eine Emailadresse angegeben haben, werden Sie benachrichtigt wenn die Registrierung abgeschlossen ist'; 
 $lang['Your gallery is already registered in Piwigo Showcase, <a href="%s">see it →</a>'] = 'Ihre Galerie ist nun registriert im Piwigo Showcase, <a href="%s">Sachauen Sie es sich an →</a>'; 
