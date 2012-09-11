f04158f33652eb42076e7216c83b46d19af14000
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-09-11 18:43:05 +0000

    [showcase_subscribe] Update th_TH, thanks to : pradit
    git-svn-id: http://piwigo.org/svn/extensions/showcase_subscribe@17867 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/th_TH/plugin.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Your gallery is already registered in Piwigo Showcase, <a href="%s">see it →</a>'] = 'แกลเลอรี่ของคุณมีการลงทะเบียนไว้แล้วใน Piwigo Showcase, <a href="%s">ดูได้ที่ →</a>';
+$lang['Your subscription is currently pending, if you have provided an email, you will be notified as soon as your gallery is registered'] = 'การลงทะเบียนของคุณกำลังรอการตรวจสอบเพื่ออนุมัติ, ถ้าคุณให้อีเมล์ไว้ คุณจะได้รับการแจ้งเตือนทันทีเมื่อได้ลงทะเบียนเสร็จแล้ว';
+$lang['provide it if you want to be notified when your gallery is registered'] = 'ให้ข้อมูลดังกล่าวถ้าคุณต้องการที่จะรับการแจ้งเตือนเมื่อแกลเลอรี่ของคุณมีการลงทะเบียน';
+$lang['An error has occured, no connection to %s'] = 'เกิดข้อผิดพลาด, ไม่มีการเชื่อมต่อไปยัง %s';
+$lang['Gallery Address (URL)'] = 'ที่อยู่แกลอรี่ (URL)';
+$lang['Subscribe my gallery to Piwigo Showcase'] = 'ลงทะเบียนแกลเลอรี่ของฉันใน Piwigo Showcase';
+$lang['A good description will improve your visibility in search engines'] = 'คำอธิบายที่ดีจะช่วยเพิ่มประสิทธิภาพการค้นหาเนื้อหาแกลอรี่ของคุณในเครื่องมือค้นหา';
+$lang['(example: nature, landscape, portrait)'] = '(ตัวอย่าง: ภาพธรรมชาติ, ภูมิประเทศ, ภาพบุคคล)';
+$lang['(optional)'] = '(ตัวเลือก)';
+?>
