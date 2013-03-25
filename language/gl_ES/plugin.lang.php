f40f0bf98095bf6c49fbf3aac6388bfb2e8237c2
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-25 21:24:11 +0000

    [showcase_subscribe] Add gl_ES, thanks to : damufo
    git-svn-id: http://piwigo.org/svn/extensions/showcase_subscribe@21842 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/gl_ES/plugin.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Subscribe my gallery to Piwigo Showcase'] = 'Rexistrar a miña galería no directorio Piwigo';
+$lang['Your gallery is already registered in Piwigo Showcase, <a href="%s">see it →</a>'] = 'A túa galería xa está rexistrada no directorio Piwigo, <a href="%s">míraa →</a>';
+$lang['(example: nature, landscape, portrait)'] = '(exemplo: natureza, paisaxe, retrato)';
+$lang['An error has occured, no connection to %s'] = 'Produciuse un erro, non hai conexión a %s';
+$lang['(optional)'] = '(opcional)';
+$lang['A good description will improve your visibility in search engines'] = 'Unha boa descrición mellorará a túa visibilidade nos motores de busca';
+$lang['Gallery Address (URL)'] = 'Enderezo da galería (URL)';
+$lang['Your subscription is currently pending, if you have provided an email, you will be notified as soon as your gallery is registered'] = 'Neste momento o teu rexistro está pendente, se forneciches un correo electrónico, serás notificado cando a galería estea rexistrada';
+$lang['provide it if you want to be notified when your gallery is registered'] = 'fornéceo se queres ser notificado cando a galería estea rexistrada';
+?>
