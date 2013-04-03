89c2aa94526d847368c88e735a6b2f05cdd40d26
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-04-03 13:12:03 +0000

    [showcase_subscribe] Add zh_TW, thanks to : tedx
    git-svn-id: http://piwigo.org/svn/extensions/showcase_subscribe@21993 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_TW/plugin.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['(example: nature, landscape, portrait)'] = '(例如：自然，風景，人像)';
+$lang['(optional)'] = '(可選)';
+$lang['A good description will improve your visibility in search engines'] = '很好的說明，將提高在搜索引擎中的可見度';
+$lang['An error has occured, no connection to %s'] = '發生了錯誤，沒有任何連接到%s';
+$lang['Gallery Address (URL)'] = '畫廊地址(URL)';
+$lang['Subscribe my gallery to Piwigo Showcase'] = '在Piwigo註冊我的畫廊展示台';
+$lang['Your gallery is already registered in Piwigo Showcase, <a href="%s">see it →</a>'] = '您的圖片庫中已註冊Piwigo的展示台，<a href="%s">請參見→</a>';
+$lang['Your subscription is currently pending, if you have provided an email, you will be notified as soon as your gallery is registered'] = '正在審理之中您的註冊，如果您已經提供了電子郵件地址，將盡快通知您的畫廊的註冊';
+$lang['provide it if you want to be notified when your gallery is registered'] = '它提供，如果您想要收到通知您註冊的畫廊';
+?>
