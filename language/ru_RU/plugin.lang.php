f6bcaa1ee3bbea08dbc2ec04262b31c0fc09d137
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-05-03 17:34:27 +0000

    [showcase_subscribe] Update ru_RU, thanks to : Konve
    git-svn-id: http://piwigo.org/svn/extensions/showcase_subscribe@22483 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/ru_RU/plugin.lang.php
+++ language/ru_RU/plugin.lang.php
@@ -2,7 +2,7 @@
 $lang['Subscribe my gallery to Piwigo Showcase'] = 'Внести мою галерею в Piwigo Showcase';
 $lang['Gallery Address (URL)'] = 'Адрес галереи (URL)';
 $lang['An error has occured, no connection to %s'] = 'Ошибка: нет соединения с %s';
-$lang['Your subscription is currently pending, if you have provided an email, you will be notified as soon as your gallery is registered'] = 'Ваша регистрация не завершена. Если вы предоставили адрес электронной почты, то получите письмо после регистрации гелереи';
+$lang['Your subscription is currently pending, if you have provided an email, you will be notified as soon as your gallery is registered'] = 'Ваша регистрация не завершена. Если вы предоставили адрес электронной почты, то получите письмо после регистрации галереи';
 $lang['Your gallery is already registered in Piwigo Showcase, <a href="%s">see it →</a>'] = 'Ваша галерея уже зарегистрирована в Piwigo Showcase, <a href="%s">посмотреть →</a>';
 $lang['A good description will improve your visibility in search engines'] = 'Хорошее описание увеличит индексируемость галереи поисковыми машинами';
 $lang['provide it if you want to be notified when your gallery is registered'] = 'Укажите адрес, если хотите получить уведомление о регистрации галереи';
