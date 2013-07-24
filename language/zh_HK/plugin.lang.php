43d2c2124f6c3001d625551740b951a304117872
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-07-24 05:59:14 +0000

    [showcase_subscribe] Add zh_HK, thanks to : gurunited
    git-svn-id: http://piwigo.org/svn/extensions/showcase_subscribe@24025 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_HK/plugin.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['provide it if you want to be notified when your gallery is registered'] = '請提供如果你想你的畫廊註冊完成後通知你';
+$lang['Your subscription is currently pending, if you have provided an email, you will be notified as soon as your gallery is registered'] = '你的註冊申請仍在等待審批, 如果你提供電郵地址, 當你的畫廊註冊完成後將會收到通知.';
+$lang['(example: nature, landscape, portrait)'] = '(例子: 不變, 橫向, 直向)';
+$lang['(optional)'] = '(隨意)';
+$lang['A good description will improve your visibility in search engines'] = '一個好的形容可以改善在搜尋引擎的出現次數';
+$lang['An error has occured, no connection to %s'] = '發生錯誤,  不能連接 %s';
+$lang['Gallery Address (URL)'] = '畫廊地址(網址)';
+$lang['Subscribe my gallery to Piwigo Showcase'] = '註冊我的畫廊在Piwigo陳列室';
+$lang['Your gallery is already registered in Piwigo Showcase, <a href="%s">see it →</a>'] = '你的畫廊已經註冊在Piwigo陳列室, <a href="%s">自這裡 →</a>';
+?>
