aa4d4a9d2ff8800d995f2b42cb21e465910ca096
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-08-12 13:01:57 +0000

    [showcase_subscribe] Update ta_IN, thanks to : saitjaleel
    git-svn-id: http://piwigo.org/svn/extensions/showcase_subscribe@24154 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ta_IN/plugin.lang.php
@@ -0,0 +1,35 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Gallery Address (URL)'] = 'புகைப்படங்கள் முகவரி (URL)';
+$lang['Subscribe my gallery to Piwigo Showcase'] = 'பதிவு செய்து என் படத்தொகுப்பில் Piwigo முன்னிலைப்படுத்தல்';
+$lang['Your gallery is already registered in Piwigo Showcase, <a href="%s">see it →</a>'] = 'உங்கள் கேலரி ஏற்கனவே Piwigo முன்னிலைப்படுத்தல் பதிவாகியிருக்கிறது, <a href="%s">அதை காண →</a>';
+$lang['Your subscription is currently pending, if you have provided an email, you will be notified as soon as your gallery is registered'] = 'உங்கள் பதிவு தற்போது நிலுவையில் உள்ளது,நீங்கள் ஒரு மின்னஞ்சல் வழங்கிய என்றால்,
+
+நீங்கள் விரைவில் உங்கள் கேலரி பதிவுசெய்யப்பட்டுள்ளது என்று அறிவிக்கப்படும்';
+$lang['provide it if you want to be notified when your gallery is registered'] = 'நீங்கள் விரும்பினால் அதனை வழங்க 
+உங்கள் கேலரி பதிவு செய்யப்பட்ட போது தெரிவிக்கப்படும்';
+$lang['An error has occured, no connection to %s'] = 'ஒரு பிழை ஏற்பட்டுள்ளது,இணைப்பு இல்லாத % s ';
+$lang['A good description will improve your visibility in search engines'] = 'ஒரு சிறந்த விளக்கம் தேடல் இயந்திரங்கள் உங்கள் காட்சித்தன்மையை மேம்படுத்து';
+$lang['(optional)'] = '(விரும்பினால்)';
+$lang['(example: nature, landscape, portrait)'] = '(உதாரணம்: இயற்கை, நிலத்தோற்றம், ஓவியம்)';
+?>
