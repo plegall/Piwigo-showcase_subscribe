06fdfd90a9a48f930af8b8cfecf219100bb97f71
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-09-16 20:54:44 +0000

    [showcase_subscribe] Add eo_EO, thanks to : sylvbarrier
    git-svn-id: http://piwigo.org/svn/extensions/showcase_subscribe@24488 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/eo_EO/plugin.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Your subscription is currently pending, if you have provided an email, you will be notified as soon as your gallery is registered'] = 'Via registrado estas okazanta; se vi provizis retadreson, vi ricevos sciigon ekde via galerio registriĝas';
+$lang['provide it if you want to be notified when your gallery is registered'] = 'provizu ĝin se vi deziras ricevi sciigon kiam via galerio registriĝos';
+$lang['Your gallery is already registered in Piwigo Showcase, <a href="%s">see it →</a>'] = 'Via galerio jam registriĝis en la montrolisto de Piwigo, <a href="%s">vidu ĝin →</a>';
+$lang['Subscribe my gallery to Piwigo Showcase'] = 'Registri mian galerion al la montrolisto de Piwigo';
+$lang['Gallery Address (URL)'] = 'Adreso de la galerio (URL)';
+$lang['A good description will improve your visibility in search engines'] = 'Bona priskribo plibonigos via videbleco en serĉiloj';
+$lang['An error has occured, no connection to %s'] = 'Eraro okazis, neniu konekto al %s';
+$lang['(optional)'] = '(opcia)';
+$lang['(example: nature, landscape, portrait)'] = '(ekzemplo: naturo, pejzaĝo, portreto)';
+?>
