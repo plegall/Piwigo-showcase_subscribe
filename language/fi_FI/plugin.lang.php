76b4bf502c5bb3ccaa9ea9915c1fd754e4da28f3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-11-11 19:55:41 +0000

    [showcase_subscribe] Add fi_FI, thanks to : lrd
    git-svn-id: http://piwigo.org/svn/extensions/showcase_subscribe@25460 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/fi_FI/plugin.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['(example: nature, landscape, portrait)'] = '(esimerkkejä: luonto, maisema, muotokuva)';
+$lang['(optional)'] = '(ei pakollinen)';
+$lang['A good description will improve your visibility in search engines'] = 'Hyvä kuvaus parantaa näkyvyyttä hakukoneissa';
+$lang['An error has occured, no connection to %s'] = 'Tapahtui virhe, yhteys %s:n epäonnistui';
+$lang['Gallery Address (URL)'] = 'Gallerian osoite (URL)';
+$lang['Subscribe my gallery to Piwigo Showcase'] = 'Rekisteröi galleriani Piwigon Näyteikkunaan';
+$lang['Your gallery is already registered in Piwigo Showcase, <a href="%s">see it →</a>'] = 'Galleriasi on jo rekisteröity Piwigon Näyteikkunaan, <a href="%s">näet sen klikkaamalla tästä</a>';
+$lang['Your subscription is currently pending, if you have provided an email, you will be notified as soon as your gallery is registered'] = 'Rekisteröintisi odottaa edelleen prosessointia. Jos olet määrittänyt sähköpostiosoitteen, saat huomautuksen kun rekisteröinti on valmis.';
+$lang['provide it if you want to be notified when your gallery is registered'] = 'lisää, jos haluat saada huomautuksen gallerian rekisteröinnin valmistumisesta.';
+?>
