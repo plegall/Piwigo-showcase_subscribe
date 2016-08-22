cf5e07be1dfc8f745ea9763cc81ba7019e2a278c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2016-08-22 06:14:38 +0200

    Update nl_NL, thanks to watertoren5044

--- language/nl_NL/plugin.lang.php
+++ language/nl_NL/plugin.lang.php
@@ -1,11 +1,10 @@
 <?php
-$lang['Subscribe my gallery to Piwigo Showcase'] = 'Meldt mijn galerie aan bij Piwigo Showcase'; 
+$lang['Subscribe my gallery to Piwigo Showcase'] = 'Meld mijn galerie aan bij Piwigo Showcase';
 $lang['Gallery Address (URL)'] = 'Galerie-adres (URL)'; 
-$lang['An error has occured, no connection to %s'] = 'Er is een fout opgetreden, geen verbinding met %s'; 
-$lang['Your subscription is currently pending, if you have provided an email, you will be notified as soon as your gallery is registered'] = 'Uw aanmelding is momenteel in behandeling. Indien u een email-adres heeft opgegeven krijgt u bericht zodra uw galerie is geregistreerd'; 
-$lang['Your gallery is already registered in Piwigo Showcase, <a href="%s">see it →</a>'] = 'Uw galerie is reeds aangemeld bij Piwigo Showcase, <a href="%s">bekijk de galerie →</a>'; 
-$lang['A good description will improve your visibility in search engines'] = 'Een goede beschrijving van uw galerie zal de zichtbaarheid bij zoekmachines vergroten'; 
-$lang['provide it if you want to be notified when your gallery is registered'] = 'Geef uw email-adres indien u gewaarschuwd wenst te worden wanneer uw galerie is ingeschreven'; 
+$lang['An error has occured, no connection to %s'] = 'Er is een fout opgetreden. Geen verbinding met %s';
+$lang['Your subscription is currently pending, if you have provided an email, you will be notified as soon as your gallery is registered'] = 'Jouw aanmelding is momenteel in behandeling. Als je een email-adres hebt opgegeven krijg je bericht zodra jouw galerie is geregistreerd';
+$lang['Your gallery is already registered in Piwigo Showcase, <a href="%s">see it →</a>'] = 'Jouw galerie is al aangemeld bij Piwigo Showcase, <a href="%s">bekijk de galerie →</a>';
+$lang['A good description will improve your visibility in search engines'] = 'Een goede beschrijving van jouw galerie zal de zichtbaarheid bij zoekmachines vergroten';
+$lang['provide it if you want to be notified when your gallery is registered'] = 'Geef je email-adres als je gewaarschuwd wilt worden als uw galerie is ingeschreven';
 $lang['(optional)'] = '(optioneel)';
 $lang['(example: nature, landscape, portrait)'] = '(bijvoorbeeld: natuur, landschap, portret)';
-?>
