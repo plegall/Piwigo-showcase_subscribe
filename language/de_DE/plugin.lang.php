c0a170d63f66fd19b20328752b2ce241fa57653d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2017-04-04 06:54:36 +0200

    Update de_DE, thanks to e-dschungel

--- language/de_DE/plugin.lang.php
+++ language/de_DE/plugin.lang.php
@@ -2,10 +2,9 @@
 $lang['Subscribe my gallery to Piwigo Showcase'] = 'Registriere meine Galerie bei Piwigo Showcase'; 
 $lang['Gallery Address (URL)'] = 'Galerieadresse (URL)'; 
 $lang['An error has occured, no connection to %s'] = 'Ein Fehler ist aufgetreten, %s ist nicht erreichbar'; 
-$lang['Your subscription is currently pending, if you have provided an email, you will be notified as soon as your gallery is registered'] = 'Ihre Registrierung ist momentan im Wartestatus, wenn Sie eine Emailadresse angegeben haben, werden Sie benachrichtigt wenn die Registrierung abgeschlossen ist'; 
-$lang['Your gallery is already registered in Piwigo Showcase, <a href="%s">see it →</a>'] = 'Ihre Galerie ist nun registriert im Piwigo Showcase, <a href="%s">Sachauen Sie es sich an →</a>'; 
-$lang['A good description will improve your visibility in search engines'] = ' Eine gute Beschreibung verbessert Ihre Auflistung in den Suchmaschinen '; 
+$lang['Your subscription is currently pending, if you have provided an email, you will be notified as soon as your gallery is registered'] = 'Ihre Registrierung ist momentan im Wartestatus. Wenn Sie eine Emailadresse angegeben haben, werden Sie benachrichtigt, wenn die Registrierung abgeschlossen ist';
+$lang['Your gallery is already registered in Piwigo Showcase, <a href="%s">see it →</a>'] = 'Ihre Galerie ist nun im Piwigo Showcase registriert, <a href="%s">Schauen Sie es sich an →</a>';
+$lang['A good description will improve your visibility in search engines'] = 'Eine gute Beschreibung verbessert Ihre Auflistung in den Suchmaschinen ';
 $lang['provide it if you want to be notified when your gallery is registered'] = 'tragen Sie sie ein, wenn Sie nach fertiggestellter Registrierung benachrichtigt werden wollen'; 
 $lang['(optional)'] = '(optional)';
 $lang['(example: nature, landscape, portrait)'] = '(z.B.: Natur, Landschaft, Porträt)';
-?>
